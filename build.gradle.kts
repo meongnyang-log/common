dab6d9762b0335a379e321d0ad23829202c5b2a5
Joojinsung1017 <hanro101707@gmail.com>
2025-03-18 04:19:22 +0900

    auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     kotlin("jvm") version "1.9.25"
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
     id("maven-publish")
 }
 
 group = "com.back"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
@@ -12,17 +14,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:3.4.3")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.2.4")
-    implementation("org.springframework.boot:spring-boot-starter-security:3.4.3")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.3")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.3")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
-    testImplementation("org.springframework.security:spring-security-test:3.4.3")
+    testImplementation("org.springframework.security:spring-security-test")
 
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
