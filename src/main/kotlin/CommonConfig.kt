ef58aa2cb1ead0cf5cb26d7dd7807668b3b110ed
Joojinsung1017 <hanro101707@gmail.com>
2025-03-18 03:39:46 +0900

    auth

--- src/main/kotlin/CommonConfig.kt
+++ src/main/kotlin/CommonConfig.kt
@@ -1,14 +1,16 @@
 package auth.config
 
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.boot.autoconfigure.domain.EntityScan
 
-@Configuration
+@AutoConfiguration
 @EnableJpaRepositories(basePackages = ["auth.repository"])
 @EntityScan(basePackages = ["auth.entity"])
 @ComponentScan(basePackages = ["auth"])
 @EnableConfigurationProperties(AuthProperties::class)
+@ConditionalOnMissingBean(CommonConfig::class)
 open class CommonConfig
