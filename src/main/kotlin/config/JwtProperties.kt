1bea2d8067c98b144440b501f77b17394961ded7
Joojinsung1017 <hanro101707@gmail.com>
2025-03-18 22:17:40 +0900

    auth

--- src/main/kotlin/config/JwtProperties.kt
+++ src/main/kotlin/config/JwtProperties.kt
@@ -2,13 +2,9 @@ package config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "auth.jwt")
-data class JwtProperties (
-    val secretKey:String,
-    val accessTokenExpiration:Long,
-    val refreshTokenExpiration:Long,
+@ConfigurationProperties("auth.jwt")
+data class JwtProperties(
+    val secretKey: String = "",
+    val accessTokenExpiration: Long = 3600000L,
+    val refreshTokenExpiration: Long = 86400000L
 )
-
-
-
-
